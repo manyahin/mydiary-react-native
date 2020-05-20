2af3564300d6da094fe0dc655707a022a6ee70cc
Ilya Manyahin <i.manyahin@gmail.com>
2020-05-21 02:44:13 +0300

    start calendar

--- screens/Calendar.jsx
+++ screens/Calendar.jsx
@@ -1,17 +1,96 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View } from 'react-native';
 import { Text } from 'react-native-elements';
+import moment from 'moment';
 
 import * as theme from '../util/theme';
 
 import TopBar from '../components/TopBar';
+import { DbContext } from '../stores/db';
+import { render } from 'react-dom';
 
 export default function CalendarScreen({ navigation }) {
+
+    const Db = React.useContext(DbContext);
+
+    const [calendar, setCalendar] = React.useState({});
+
+    useEffect(() => {
+        Db.getOnlyNotesDates()
+            .then(data => {
+                // make list of dates with count of posts
+                return data.map(el => el.created_at)
+                    .reduce((acc, curr) => {
+                        const day = moment(curr).startOf('day').format('YYYY-MM-DD');
+
+                        if (acc.has(day)) acc.set(day, acc.get(day) + 1);
+                        else acc.set(day, 0);
+
+                        return acc;
+                    }, new Map());
+            })
+            .then(activeDates => {
+                if (!activeDates.size) return activeDates;
+
+                // fill all the monthes
+                let currDay = moment(activeDates.keys().next().value).startOf('month');
+                const lastDay = moment().endOf('month');
+
+                do {
+                    let currDayFormat = currDay.format('YYYY-MM-DD');
+
+                    if (!activeDates.has(currDayFormat)) {
+                        activeDates.set(currDayFormat, 0);
+                    }
+
+                    currDay = currDay.add(1, 'day');
+                } while (currDay < lastDay);
+
+                return activeDates;
+            })
+            .then(dates => {
+                // build calendar object
+                let calendar = {};
+
+                for (const [dayFormat, cnt] of dates) {
+                    const date = moment(dayFormat);
+
+                    let year = date.format('YYYY');
+                    let month = date.format('MMMM');
+                    let day = date.format('D');
+
+                    if (!calendar[year]) calendar[year] = {};
+                    if (!calendar[year][month]) calendar[year][month] = {};
+
+                    calendar[year][month][day] = {
+                        format: dayFormat,
+                        cnt: cnt
+                    };
+                }
+
+                return calendar;
+            })
+            .then(calendar => {
+                console.log(calendar);
+                setCalendar(calendar);
+            })
+    }, []);
+
+
+    const renderCalendar = (calendar) => {
+        return Object.entries(calendar).map(([year, monthes]) => {
+            let ret = (<Text>{year}</Text>);
+            return ret + renderMonthes(monthes);
+        });
+    };
+
+ 
     return (
         <View style={theme.baseContainer}>
-            <TopBar/>
+            <TopBar />
             <View>
                 <Text>In developming...</Text>
+                {renderCalendar(calendar)}
             </View>
         </View>
     )
--- stores/db.js
+++ stores/db.js
@@ -11,14 +11,38 @@ const remoteDBProvider = {
   getNotes: async (sort = 'asc', limit = 50) => {
     const userToken = await AsyncStorage.getItem('@user_token');
 
-    const url = `Notes?filter=${JSON.stringify({ order: `created_at ${sort}`, limit })}`;
+    const filter = { order: `created_at ${sort}`, limit };
+    const url = `Notes?filter=${JSON.stringify(filter)}`;
 
     try {
-      const data = await axios.get(url, {
+      const { data } = await axios.get(url, {
         headers: { 'Authorization': userToken }
       })
 
-      return data.data;
+      return data;
+    }
+    catch (err) {
+      console.error(err);
+
+      return [];
+    }
+  },
+  getOnlyNotesDates: async () => {
+    const userToken = await AsyncStorage.getItem('@user_token');
+    
+    const filter = {
+      fields: {
+        created_at: true
+      },
+      order: 'created_at ASC'
+    };
+
+    try {
+      const { data } = await axios.get('Notes?filter=' + JSON.stringify(filter), {
+        headers: { 'Authorization': userToken }
+      });
+
+      return data;
     }
     catch (err) {
       console.error(err);
@@ -34,18 +58,22 @@ const remoteDBProvider = {
     const userToken = await AsyncStorage.getItem('@user_token');
 
     try {
-      const data = await axios.post('Notes', note, {
+      const { data } = await axios.post('Notes', note, {
         headers: { 'Authorization': userToken }
       })
 
-      return data.data;
+      return data;
     }
     catch (err) {
       console.error(err);
 
       return [];
     }
-  }
+  },
+  deleteAllNotes: async () => {
+    // realize me please
+    return {};
+  },
 };
 
 const localDbProvider = {
@@ -65,7 +93,7 @@ const localDbProvider = {
     catch (err) {
       console.error('Error while adding new note', err);
     }
-    
+
     return {
       ...note,
       count_symbols: note.body.length
@@ -75,6 +103,10 @@ const localDbProvider = {
     const notes = JSON.parse(await AsyncStorage.getItem('@notes')) || [];
 
     return notes;
+  },
+  getOnlyNotesDates: async () => {
+    // realize me please
+    return {};
   },
   // probably you don't want to call it
   deleteAllNotes: async () => {
